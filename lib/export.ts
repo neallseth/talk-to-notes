a2573b34b0187671b92fdb92dc6044d486b343f5
neallseth <32910906+neallseth@users.noreply.github.com>
2024-06-24 00:00:11 -0400

    adding cal export script

--- lib/export.ts
+++ lib/export.ts
@@ -2,13 +2,18 @@ import { spawnSync } from "bun";
 import path from "path";
 
 // Path to your AppleScript file
-const appleScriptPath = path.join(import.meta.dir, "export_notes.scpt");
+const notesExportScriptPath = path.join(import.meta.dir, "export_notes.scpt");
+const calExportScriptPath = path.join(import.meta.dir, "export_cal.scpt");
 // Path to the export directory
-const exportDir = path.join(import.meta.dir, "../notes/raw");
+const notesExportDir = path.join(import.meta.dir, "../notes/raw");
+const calExportDir = path.join(import.meta.dir, "../cal/raw");
 
-// Function to execute the AppleScript with a parameter
-function runExportScript() {
-  const result = spawnSync(["osascript", appleScriptPath, exportDir]);
+function exportNotes() {
+  const result = spawnSync([
+    "osascript",
+    notesExportScriptPath,
+    notesExportDir,
+  ]);
 
   const stdout = result.stdout.toString().trim();
   const stderr = result.stderr.toString();
@@ -20,4 +25,18 @@ function runExportScript() {
   }
 }
 
-runExportScript();
+function exportCal() {
+  const result = spawnSync(["osascript", calExportScriptPath, calExportDir]);
+
+  const stdout = result.stdout.toString().trim();
+  const stderr = result.stderr.toString();
+
+  if (stderr) {
+    console.error(`Error: ${stderr}`);
+  } else {
+    console.log(`Success! Exported ${stdout} events.`);
+  }
+}
+
+exportNotes();
+exportCal();
